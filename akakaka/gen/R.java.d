5d956cda2f66ba13217a86faf0b83b3ebe41a261
raimis001 <raimis0001@gmail.com>
2012-02-12 14:45:18 +0200

    First

---
+++ akakaka/gen/R.java.d
@@ -0,0 +1,6 @@
+K:\www\akka\akakaka\gen\akakaka\com\R.java \
+ : K:\www\akka\akakaka\res\layout\main.xml \
+K:\www\akka\akakaka\res\values\strings.xml \
+K:\www\akka\akakaka\res\xml\phonegap.xml \
+K:\www\akka\akakaka\res\xml\plugins.xml \
+K:\www\akka\akakaka\AndroidManifest.xml \
---
+++ akakaka/src/akakaka/com/MainActivity.java
@@ -0,0 +1,19 @@
+package akakaka.com;
+
+import android.os.Bundle;
+import com.phonegap.DroidGap;
+
+public class MainActivity extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+        
+        //WebView engine = (WebView) findViewById(R.id.web_engine);  
+        //engine.loadUrl("file:///android_asset/www/index.html"); 
+
+    }
+}
